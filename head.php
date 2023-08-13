54f8303d3d6b337756a575dba0d5076c13065639
Ayadsherzad182 <142124514+Ayadsherzad182@users.noreply.github.com>
2023-08-13 18:04:39 +0300

    Add files via upload

---
+++ head.php
@@ -0,0 +1,24 @@
+<!DOCTYPE htmL>
+ <htm1> 
+<head> <meta charset="utf-8">
+<meta name="viewport" content=" width=device-width, initial-scale=1"> <title>TODO< /title>
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/ bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhF1dvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqy12QvZ6jIW3" crossorigin="anonymous">
+</head>
+ <body>
+
+<nav class="navbar navbar-expand-1g navbar-dark bg-dark">
+<div class="container-fluid">
+<a class="navbar-brand" href=" /index. php" ›TODO</a>
+<button class="navbar-toggler" type="button" data-bs-toggle=" collapse" data-bs-target=" #navbarSupportedContent" aria-controls=" navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"> </span> </button>
+<div class="collapse navbar-collapse" id=" navbarSupportedContent">
+
+<ul class="navbar-nav me-auto mb-2 mb-1g-0">
+<li class="nav-item">
+<a href="./index.php">Home</a> </li> <li class="nav-item"> <a href="./ addTodo. php" >Home</a>
+</li>
+<li class="nav-item">
+<a href="./addTodo.php">Home</a>
+</ul>
+</div>
+<div>
